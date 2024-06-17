cdcef2e9b3112ddbd50fa5c9683fd08be86973e3
stepannemejc <stepannemejc@users.noreply.github.com>
2024-06-17 08:43:48 +0200

    changes

--- src/PohodaExportGenerator.php
+++ src/PohodaExportGenerator.php
@@ -4,24 +4,14 @@ namespace Apexmediacz;
 
 class PohodaExportGenerator
 {
+	const VAT_NONE = "none";
+	const VAT_HIGH = "high";
+	const VAT_LOW = "low";
+	const VAT_THIRD = "third";
+
 	private $vat_high_rate = 21;
 	public function generateXml($data, $toScreen = false)
 	{
-		// $pohoda = new \Pohoda\Export($data->accounting_unit_identification_number);
-		// foreach ($data->invoices as $invoice) {
-		// 	$createdInvoice = $this->createInvoice($invoice);
-		// 	if ($createdInvoice) {
-		// 		if ($createdInvoice->isValid()) {
-		// 		}
-		// 		$pohoda->addInvoice($createdInvoice);
-		// 	}
-		// }
-
-		// if ($toScreen) {
-		// 	return $pohoda->exportAsXml(time(), 'popis', date("Y-m-d_H-i-s"));
-		// }
-		// return $pohoda->exportAsString(time(), 'popis', date("Y-m-d_H-i-s"));
-
 		$pohoda = new \Apexmediacz\Document($data->accounting_unit_identification_number);
 		$invoices = new \Apexmediacz\Invoice;
 		$invoicesData = [];
@@ -39,58 +29,6 @@ class PohodaExportGenerator
 			return $dataPackXml;
 		}
 		return $dataPackXml;
-	}
-
-	public function createInvoice($invoiceData)
-	{
-		try {
-			$invoice = new \Pohoda\Invoice($invoiceData->invoice_id);
-			$invoice->setText($invoiceData->description);
-			$invoiceTotals = $this->calculateTotalInvoiceAmounts($invoiceData);
-			$invoice->setBank($invoiceData->payment->bank_name);
-
-			$invoice->setPriceLow($invoiceTotals->low->totalWithoutVat);
-			$invoice->setPriceLowVAT($invoiceTotals->low->totalVat);
-			$invoice->setPriceLowSum($invoiceTotals->low->totalWithVat);
-
-			$invoice->setPriceHigh($invoiceTotals->high->totalWithoutVat);
-			$invoice->setPriceHightVAT($invoiceTotals->high->totalVat);
-			$invoice->setPriceHighSum($invoiceTotals->high->totalWithVat);
-
-			$invoice->setWithVat(true);
-
-			foreach ($invoiceData->items as $item) {
-				$invoice->addItem($this->createInvoiceItem($item));
-			}
-
-			$invoice->setVariableNumber($invoiceData->payment->variable_symbol);
-			$invoice->setDateCreated($invoiceData->issue_date);
-			$invoice->setDateTax($invoiceData->tax_point_date);
-			$invoice->setDateDue($invoiceData->payment->payment_due_date);
-
-			$invoice->setProviderIdentity($this->createPartyItem($invoiceData->supplier));
-
-			$customerAddress = $invoice->createCustomerAddress($this->createPartyItem($invoiceData->customer));
-			$invoice->setCustomerAddress($customerAddress);
-
-			return $invoice;
-		} catch (\Pohoda\InvoiceException $e) {
-			//print_r($e);
-		} catch (\InvalidArgumentException $e) {
-			//print_r($e);
-		}
-	}
-
-	public function createInvoiceItem($item)
-	{
-		$invoiceItem = new \Pohoda\InvoiceItem();
-		$invoiceItem->setText($item->name);
-		$invoiceItem->setQuantity($item->quantity);
-		$invoiceItem->setUnit($item->unit_label);
-		$invoiceItem->setUnitPrice($item->total_item_price_base);
-		$invoiceItem->setRateVAT($this->getInvoiceItemVatRate($item));
-		$invoiceItem->setPayVAT(false); //zadaná cena je bez daně
-		return $invoiceItem;
 	}
 
 	/* HELPER FUNCTIONS */
@@ -146,6 +84,6 @@ class PohodaExportGenerator
 
 	public function getInvoiceItemVatRate($item)
 	{
-		return ($item->vat_rate_percent == 21 ? \Pohoda\InvoiceItem::VAT_HIGH : \Pohoda\InvoiceItem::VAT_LOW);
+		return ($item->vat_rate_percent == 21 ? self::VAT_HIGH : self::VAT_LOW);
 	}
 }
